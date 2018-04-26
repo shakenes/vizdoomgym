30d6dd91afd1e07ff9655eee51601a99fa1a032c
Simon <simon.hakenes@tu-dortmund.de>
2018-04-26 16:18:23 +0200

    add vizdoom requirement

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='vizdoomgym',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym', 'vizdoom']  # And any other dependencies foo needs
       )
