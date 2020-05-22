3f4b38ddb4a9acb5615ca35d224deb32ac1cf69d
Simon <simon.hakenes@tu-dortmund.de>
2020-05-22 22:30:11 +0200

    add more testing

--- tests/BasicTest.py
+++ tests/BasicTest.py
@@ -5,15 +5,33 @@ import vizdoomgym
 
 class BasicTest(unittest.TestCase):
     def test_basic(self):
-        env = gym.make("VizdoomBasic-v0", test_arg="something")
+        env = gym.make("VizdoomBasic-v0")
         self.assertIsInstance(env, gym.Env)
         state = env.reset()
         self.assertEqual(len(state.shape), 3)
+        self.assertTrue(state.shape, env.observation_space.shape)
         state, reward, done, info = env.step(env.action_space.sample())
         self.assertEqual(len(state.shape), 3)
+        self.assertTrue(state.shape, env.observation_space.shape)
         self.assertIsNotNone(reward)
         self.assertIsInstance(done, bool)
         env.render()
+        env.close()
+
+    def test_kwargs(self):
+        env = gym.make("VizdoomBasic-v0", object_labels=True)
+        # self.assertTrue(env.unwrapped.object_labels)
+        self.assertIsInstance(env, gym.Env)
+        state = env.reset()
+        self.assertEqual(len(state.shape), 3)
+        self.assertTrue(state.shape, env.observation_space.shape)
+        state, reward, done, info = env.step(env.action_space.sample())
+        self.assertEqual(len(state.shape), 3)
+        self.assertTrue(state.shape, env.observation_space.shape)
+        self.assertIsNotNone(reward)
+        self.assertIsInstance(done, bool)
+        env.render()
+        env.close()
 
 
 if __name__ == "__main__":
