658d57fcae9af708f7a5046edf64622d7da2bb9d
Simon <simon.hakenes@tu-dortmund.de>
2020-05-24 13:30:02 +0200

    adjust tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,7 +7,7 @@ class BasicTest(unittest.TestCase):
     def test_basic(self):
         env = gym.make("VizdoomBasic-v0")
         self.assertFalse(env.unwrapped.depth)
-        self.assertFalse(env.unwrapped.objects)
+        self.assertFalse(env.unwrapped.labels)
         self.assertIsInstance(env, gym.Env)
         state = env.reset()
         self.assertEqual(len(state.shape), 3)
@@ -21,9 +21,9 @@ class BasicTest(unittest.TestCase):
         env.close()
 
     def test_kwargs(self):
-        env = gym.make("VizdoomDefendCenter-v0", depth=True, objects=True)
+        env = gym.make("VizdoomHealthGathering-v0", depth=True, labels=True)
         self.assertTrue(env.unwrapped.depth)
-        self.assertTrue(env.unwrapped.objects)
+        self.assertTrue(env.unwrapped.labels)
         self.assertIsInstance(env, gym.Env)
         state = env.reset()
         self.assertEqual(len(state.shape), 3)
